ad5da6ee5ee39ac654a9c5abd387c94a8bcae218
EtienneJavelle <83096541+Omadel@users.noreply.github.com>
2021-08-27 13:03:08 +0200

    Update Libraries

--- Scripts/ScriptableObjects/SamplableLibrary.cs
+++ Scripts/ScriptableObjects/SamplableLibrary.cs
@@ -1,29 +1,9 @@
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 namespace Etienne {
-    [CreateAssetMenu(fileName = "Library", menuName = "Base Library", order = 5)]
-    internal class SamplableLibrary : InitializableScriptableObject {
-        public static List<Object> Library => staticLibrary;
-        private static List<Object> staticLibrary;
-        [SerializeField] private List<Object> library;
+    [CreateAssetMenu(fileName = "Library", menuName = "Samplable Library", order = 5)]
+    internal class SamplableLibrary : ScriptableObjectLibrary<Object> {
         [SerializeField] private Object sampleType;
-
-        private System.Type Type => sampleType.GetType();
-
-        public override void Initialize() {
-#if UNITY_EDITOR
-            library.Clear();
-            string[] assets = AssetDatabase.FindAssets($"t:{Type.FullName}");
-            foreach(string asset in assets) {
-                string path = AssetDatabase.GUIDToAssetPath(asset);
-                Object item = AssetDatabase.LoadAssetAtPath(path, Type);
-                library.Add(item);
-                Debug.Log($"{item} Initialized");
-            }
-            staticLibrary = library;
-#endif
-        }
+        protected override System.Type Type => sampleType.GetType();
     }
 }
--- Scripts/ScriptableObjects/ScriptableObjectLibrary.cs
+++ Scripts/ScriptableObjects/ScriptableObjectLibrary.cs
@@ -3,22 +3,28 @@ using UnityEditor;
 #endif
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
 
 namespace Etienne {
-    public class ScriptableObjectLibrary<T> : InitializableScriptableObject where T : ScriptableObject {
+    public class ScriptableObjectLibrary<T> : InitializableScriptableObject where T : Object {
         public static List<T> Library => staticLibrary;
         protected static List<T> staticLibrary;
         [SerializeField] protected List<T> library;
+        protected virtual System.Type Type => typeof(T);
+
         public override void Initialize() {
 #if UNITY_EDITOR
             library.Clear();
-            string[] assets = AssetDatabase.FindAssets($"t:{typeof(T).FullName}");
+            StringBuilder message = new StringBuilder();
+            message.Append($"{GetType().FullName} Initialize {Type.FullName} \r\n");
+            string[] assets = AssetDatabase.FindAssets($"t:{Type.FullName}");
             foreach(string asset in assets) {
                 string path = AssetDatabase.GUIDToAssetPath(asset);
-                T item = AssetDatabase.LoadAssetAtPath<T>(path);
-                library.Add(item);
-                Debug.Log($"{item} Initialized");
+                Object item = AssetDatabase.LoadAssetAtPath(path, Type);
+                library.Add(item as T);
+                message.Append($"{item}, ");
             }
+            Debug.Log($"{message}\r\nInitialized");
             staticLibrary = library;
 #endif
         }
