b8862586b5b270dfb53bbed5ce55869e9d3dd5e1
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2022-09-29 13:45:00 +0200

    Update StopTime.cs
    Working Stop time

--- Runtime/GameFeedBacks/StopTime.cs
+++ Runtime/GameFeedBacks/StopTime.cs
@@ -11,21 +11,23 @@ namespace Etienne.Feedback
         [SerializeField] private float timer;
 
         protected override void Execute(GameObject gameObject)
-        {
+	    {
             Time.timeScale = 0;
         }
 
         public override async Task ExecuteAsync(GameObject gameObject)
         {
-            float oldScale = Time.timeScale;
-            await base.ExecuteAsync(gameObject);
+	        float oldScale = Time.timeScale;
+	        Execute(gameObject);
+	        await Task.Delay((int)(timer * 1000));
             Time.timeScale = oldScale;
         }
 
         public override IEnumerator ExecuteCoroutine(GameObject gameObject)
         {
-            float oldScale = Time.timeScale;
-            yield return base.ExecuteCoroutine(gameObject);
+	        float oldScale = Time.timeScale;
+	        Execute(gameObject);
+	        yield return new WaitForSecondsRealtime(timer);
             Time.timeScale = oldScale;
         }
 
