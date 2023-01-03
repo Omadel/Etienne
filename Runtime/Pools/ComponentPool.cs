b3124b21594f92a05128a6d2c496103a1bc8f1aa
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2023-01-03 13:45:00 +0100

    Update ComponentPool.cs

--- Runtime/Pools/ComponentPool.cs
+++ Runtime/Pools/ComponentPool.cs
@@ -9,13 +9,49 @@ namespace Etienne.Pools
         protected GameObject _Parent;
         private T _Prefab;
 
+        protected ComponentPool() { }
+        /// <summary>
+        /// Create a pool with a prefab as a reference.
+        /// </summary>
+        /// <param name="size">The desired count of GameObject·s to instantiate.</param>
+        /// <param name="prefab">The desired prefab to instantiate.</param>
+        /// <param name="hideFlags">Optionnal, the pool's HideFlags to avoid bloat.</param>
+        /// <returns>The created pool</returns>
+        public ComponentPool(int size, T prefab, string name = "", HideFlags hideFlags = HideFlags.None, bool dontDestroyOnLoad = false)
+        {
+            queue = new Queue<T>(size);
+            CreatePool(size, name, hideFlags, prefab, dontDestroyOnLoad);
+        }
+
+        public override T Dequeue()
+        {
+            T item = queue.Dequeue();
+            item.gameObject.SetActive(true);
+            return item;
+        }
+
+        public override void Enqueue(T item)
+        {
+            if (item == null) return;
+            item.transform.parent = _Parent.transform;
+            item.gameObject.SetActive(false);
+            queue.Enqueue(item);
+        }
+
         protected override void CreatePool(int maxSize, params object[] additionnalParameters)
         {
-            string poolName = new Regex(@".[^`]$").Replace(GetType().Name, " ") + typeof(T).Name;
+            string poolName = new Regex(@".[^`]$").Replace(GetType().Name, " ");
+
+            if (additionnalParameters.Length >= 1)
+            {
+                string paramName = (string)additionnalParameters[0];
+                poolName += paramName == "" ? typeof(T).Name : paramName;
+            }
             _Parent = new GameObject(poolName);
-            if (additionnalParameters.Length >= 1) _Parent.hideFlags = (HideFlags)additionnalParameters[0];
-            if (additionnalParameters.Length >= 2) _Prefab = (T)additionnalParameters[1];
-            if (additionnalParameters.Length >= 3 && (bool)additionnalParameters[2]) GameObject.DontDestroyOnLoad(_Parent);
+            if (additionnalParameters.Length >= 2) _Parent.hideFlags = (HideFlags)additionnalParameters[1];
+            if (additionnalParameters.Length >= 3) _Prefab = (T)additionnalParameters[2];
+            if (additionnalParameters.Length >= 4 && (bool)additionnalParameters[3]) GameObject.DontDestroyOnLoad(_Parent);
+
             string itemName = poolName.Replace("Pool", "");
             for (int i = 0; i < maxSize; i++)
             {
@@ -32,35 +68,6 @@ namespace Etienne.Pools
                     Enqueue(instance);
                 }
             }
-        }
-
-        protected ComponentPool() { }
-        /// <summary>
-        /// Create a pool with a prefab as a reference.
-        /// </summary>
-        /// <param name="size">The desired count of GameObject·s to instantiate.</param>
-        /// <param name="prefab">The desired prefab to instantiate.</param>
-        /// <param name="hideFlags">Optionnal, the pool's HideFlags to avoid bloat.</param>
-        /// <returns>The created pool</returns>
-        public ComponentPool(int size, T prefab, HideFlags hideFlags = HideFlags.None, bool dontDestroyOnLoad = true)
-        {
-            queue = new Queue<T>(size);
-            CreatePool(size, hideFlags, prefab, dontDestroyOnLoad);
-        }
-
-        public override T Dequeue()
-        {
-            T item = queue.Dequeue();
-            item.gameObject.SetActive(true);
-            return item;
-        }
-
-        public override void Enqueue(T item)
-        {
-            if (item == null) return;
-            item.transform.parent = _Parent.transform;
-            item.gameObject.SetActive(false);
-            queue.Enqueue(item);
         }
     }
 }
