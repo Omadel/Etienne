e86fc84a891baa076b0f26fa52112ccd5ef51e7e
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2023-01-09 17:58:35 +0100

    GPT Upgrade

--- Runtime/Extensions/Singleton.cs
+++ Runtime/Extensions/Singleton.cs
@@ -1,45 +1,52 @@
 ﻿using UnityEngine;
 
-namespace Etienne {
-    public abstract class Singleton<T> : MonoBehaviour where T : class {
+namespace Etienne
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : class
+    {
         public static T Instance => instance;
 
         [Header("Singleton")]
         [SerializeField] protected bool isPersistant = false;
 
-        protected virtual void Awake() {
-            if(Singleton<T>.instance != null) {
+        private static T instance;
+
+        protected Singleton() { }
+
+        protected virtual void Awake()
+        {
+            if (Singleton<T>.instance != null)
+            {
                 Debug.LogError($"There are more than one <b>\"{typeof(T).Name}\"</b> singleton.", gameObject);
                 GameObject.Destroy(this);
                 return;
             }
             Singleton<T>.instance = this as T;
 
-            if(isPersistant) {
-                if(transform.parent != null) {
+            if (isPersistant)
+            {
+                if (transform.parent != null)
+                {
                     transform.parent = null;
                     Debug.LogWarning($"Singleton <b>\"{typeof(T).Name}\"</b> is a child, it has been removed from his parent.", gameObject);
                 }
                 DontDestroyOnLoad(this);
             }
+
+            Application.quitting += ResetInstance;
         }
 
-        protected virtual void OnDestroy() {
-            if(!isPersistant) ResetInstance();
+        protected virtual void OnDestroy()
+        {
+            if (!isPersistant) ResetInstance();
         }
 
         public static void ResetInstance() => Singleton<T>.instance = null;
 
-        public void DestroyInstance() {
+        public void DestroyInstance()
+        {
             GameObject.Destroy(gameObject);
             ResetInstance();
         }
-        
-	    protected virtual void OnApplicationQuit()
-	    {
-	    	ResetInstance();
-	    }
-
-        private static T instance;
     }
 }
